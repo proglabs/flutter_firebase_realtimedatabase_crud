97642aa5d804ad6b1a0e6ec793be24d4d33555dd
Junaid <junaidk@dreamwarrior.com>
2022-09-26 21:01:51 +0500

    update data in the firebase realtime database

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -19,6 +19,9 @@ class _HomeScreenState extends State<HomeScreen> {
 
   List<Student> studentList = [];
 
+  bool updateStudent = false;
+
+
   @override
   void initState() {
     super.initState();
@@ -41,12 +44,16 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
       floatingActionButton: FloatingActionButton(onPressed: (){
+        _edtNameController.text = "";
+        _edtAgeController.text = "";
+        _edtSubjectController.text = "";
+        updateStudent = false;
         studentDialog();
       },child: const Icon(Icons.save),),
     );
   }
 
-  void studentDialog() {
+  void studentDialog({String? key}) {
     showDialog(context: context, builder: (context){
       return Dialog(
         child: Container(
@@ -65,12 +72,27 @@ class _HomeScreenState extends State<HomeScreen> {
                   "age": _edtAgeController.text.toString(),
                   "subject": _edtSubjectController.text.toString()
                 };
+
+                if(updateStudent){
+                  dbRef.child("Students").child(key!).update(data).then((value) {
+                    int index = studentList.indexWhere((element) => element.key == key);
+                    studentList.removeAt(index);
+                    studentList.insert(index,Student(key: key, studentData: StudentData.fromJson(data)));
+                    setState(() {
+
+                    });
+                    Navigator.of(context).pop();
+                  });
+                }
+                else{
+                  dbRef.child("Students").push().set(data).then((value){
+                    Navigator.of(context).pop();
+                  });
+                }
                 
-                dbRef.child("Students").push().set(data).then((value){
-                  Navigator.of(context).pop();
-                });
+
                 
-              }, child: const Text("Save Data"))
+              }, child: Text(updateStudent ? "Update Data" : "Save Data"))
             ],
           ),
         ),
@@ -88,26 +110,35 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   Widget studentWidget(Student studentList) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      padding: const EdgeInsets.all(5),
-      margin: const EdgeInsets.only(top:5,left: 10,right: 10),
-      decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), border: Border.all(color: Colors.black)),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(studentList.studentData!.name!),
-              Text(studentList.studentData!.age!),
-              Text(studentList.studentData!.subject!),
-            ],
-          ),
+    return InkWell(
+      onTap: (){
+        _edtNameController.text = studentList.studentData!.name!;
+        _edtAgeController.text = studentList.studentData!.age!;
+        _edtSubjectController.text = studentList.studentData!.subject!;
+        updateStudent = true;
+        studentDialog(key: studentList.key);
+      },
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.all(5),
+        margin: const EdgeInsets.only(top:5,left: 10,right: 10),
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), border: Border.all(color: Colors.black)),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(studentList.studentData!.name!),
+                Text(studentList.studentData!.age!),
+                Text(studentList.studentData!.subject!),
+              ],
+            ),
 
-          const Icon(Icons.delete,color: Colors.red,)
-        ],
+            const Icon(Icons.delete,color: Colors.red,)
+          ],
+        ),
       ),
     );
   }
